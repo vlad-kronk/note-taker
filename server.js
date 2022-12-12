a37c93df3e5f49e7d5b51d442f9926986fffefd5
Jacob Meyers <jacobmeyers6@outlook.com>
2022-12-11 23:02:48 -0800

    working on api post request, throwing JSON error

--- server.js
+++ server.js
@@ -3,14 +3,86 @@ const path = require('path');
 const {v4 : uuidv4} = require('uuid')
 const fs = require('fs');
 
+const notesData = require('./db/db.json');
+
 const app = express();
 const PORT = 3001;
 
+// set up the express to parse data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// serve static files
+
 app.use(express.static('public'));
 
-// for GET '*' serve the homepage
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
 // for GET 'notes' serve the notes page
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+// for GET '' serve the homepage
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+
+// handle API requests
+
+app.get('/api/notes', (req, res) => res.json(notesData));
+
+app.post('/api/notes', (req, res) => {
+  // log that request has been recieved
+  console.info(`${req.method} request recieved on port ${PORT}`);
+
+  // send response back to client
+  // res.status(201).send("Request recieved");
+
+  // initialize response var to send back to client
+  let response;
+
+  // did the user submit anything in the body of the req
+  if (req.body && req.body.title) {
+    response = {
+      status: 'success',
+      // set up a response data object with a unique ID along with the post request body content
+      data: {
+        "title": req.body.title,
+        "text": req.body.text,
+        "id": uuidv4()
+      }
+    };
+
+    // read from the database to set up an array with the current set of notes
+    fs.readFile(path.join(__dirname, 'db/db.json'), (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // initialize array with current notes
+        let notesArray = JSON.parse(data);
+
+        // push the response object with the note and id onto the end of the array
+        notesArray.push(response.data);
+
+        // write the updated array to the database
+        fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notesArray, null, 4), (err) => {
+          err ? console.log(`Note "${response.title}" has been written to file`) : console.error(err);
+        });
+      }
+    })
+
+    res.status(201).json(response);
+  } else {
+    res.status(400).json('Request body must contain at least a title');
+  }
+
+  console.log(`${response.data.title}\n${response.data.text}`);
+});
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`serving on port ${PORT}`)
+);
